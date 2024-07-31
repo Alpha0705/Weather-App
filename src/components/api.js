5caf0c87c74a961c39a202f0adc8bf50f97b56ef
AlphaArius07 <neevtshah@gmail.com>
2024-08-01 00:58:07 +0530

    Completion of Weather Forecast Website.

---
+++ src/components/api.js
@@ -0,0 +1,11 @@
+export const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '41285c61d8mshaa2053adb48abcep196392jsnd99c80a7918e',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const Weather_Api_url = "https://api.openweathermap.org/data/2.5";
+export const Weather_Api_Key = "c1d15155ce17f4e209c80f27debb13ce"
